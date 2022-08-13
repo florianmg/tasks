2feb0dffbedc29623cd0a85530f75b3bbd09a255
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2022-08-13 19:10:49 +0200

    feat(navigation): install packages + default HomeScreen + navigation setup

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default HomeScreen;
